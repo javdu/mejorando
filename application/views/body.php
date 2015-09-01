318a1dc7959190212ee42ce357a11e7af8b3c75e
javdu <javdu0113@gmail.com>
2015-09-01 11:58:12 -0300

    Se crea masterpage

---
+++ application/views/body.php
@@ -0,0 +1,6 @@
+<img src="./assets/img/la-familia-feliz.jpg" alt="Imagen" class="img-circle center-block" style="border: -webkit-box-shadow: 0px 0px 31px -16px rgba(0,0,0,0.75);
+-moz-box-shadow: 0px 0px 31px -16px rgba(0,0,0,0.75);
+box-shadow: 0px 0px 31px -16px rgba(0,0,0,0.75);; width: 250px;">
+<h1 align="center">Bienvenidos</h1>
+<h1 align="center">MEJORANDO MENTES ONLINE</h1>
+<button style="width: 200px;" type="button" class="btn btn-default btn-lg center-block">Comenzar</button>
---
+++ application/views/masterpage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="assets/css/vendor.css" rel="stylesheet" type="text/css">
+    <script src="assets/js/vendor.js"></script>
+	<title>Fundación P.A.S.S.</title>
+</head>
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Fundación P.A.S.S.</a>
+        </div>
+      </div><!-- /.container-fluid -->
+    </nav>
+    <?php include 'body.php'; ?>
+    <footer>
+        <div class="row">
+            <div class="col-xs-9 col-md-6">
+                <h4>FUNDACION P.A.S.S.</h4>
+                <p>Programa de Ayuda Solidaria Salta - Dar y Generar Ayuda Solidaria mediante proyectos
+                diseñados en Educación: (Talleres de Psicopedagogía y Fonoaudiología Gratuitos para todos
+                en la Provincia de Salta).</p>
+            </div>
+            <div class="col-xs-9 col-md-6">
+                <ul style="list-style: none;">
+                    <li>Argentina - Ciudad de Salta</li>
+                    <li>Santiago del Estero 1216 - Cod. Post. 4400</li>
+                    <li>(0387) 4211462 - (0387) 155-94-5555</li>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                </ul>
+            </div>
+        </div>
+    </footer>     
+</div>
+</body>
+</html>
