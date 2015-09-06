e418efbe1352b71c7e5482ebe5ca7b01903ebbaf
javdu <javdu0113@gmail.com>
2015-09-06 15:19:57 -0300

    Se implementa style para el masterpage

---
+++ application/views/header.php
@@ -0,0 +1,14 @@
+<!-- Header -->
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">  
+                <div class="intro-text">
+                    <div class="intro-lead-in">Bienvenidos a nuestra encuesta online!</div>
+                    <div class="intro-heading">MEJORANDO MENTES</div>
+                    <button style="width: 200px;" type="button" class="btn btn-default btn-lg center-block">Comenzar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- application/views/masterpage.php
+++ application/views/masterpage.php
@@ -2,45 +2,38 @@
 <html lang="en">
 <head>
     <link href="assets/css/vendor.css" rel="stylesheet" type="text/css">
-    <script src="assets/js/vendor.js"></script>
-	<title>Fundación P.A.S.S.</title>
+    <link href="assets/css/my.css" rel="stylesheet" type="text/css">
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <title>Fundación P.A.S.S.</title>
 </head>
 <body>
+    <?php include 'navbar.php'; ?>
+    <?php include 'header.php'; ?>
+    <br />
+    <br />
 <div class="container">
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Fundación P.A.S.S.</a>
-        </div>
-      </div><!-- /.container-fluid -->
-    </nav>
     <?php include 'body.php'; ?>
-    <footer>
-        <div class="row">
-            <div class="col-xs-9 col-md-6">
-                <h4>FUNDACION P.A.S.S.</h4>
-                <p>Programa de Ayuda Solidaria Salta - Dar y Generar Ayuda Solidaria mediante proyectos
-                diseñados en Educación: (Talleres de Psicopedagogía y Fonoaudiología Gratuitos para todos
-                en la Provincia de Salta).</p>
-            </div>
-            <div class="col-xs-9 col-md-6">
-                <ul style="list-style: none;">
-                    <li>Argentina - Ciudad de Salta</li>
-                    <li>Santiago del Estero 1216 - Cod. Post. 4400</li>
-                    <li>(0387) 4211462 - (0387) 155-94-5555</li>
-                    <li>Facebook</li>
-                    <li>Twitter</li>
-                </ul>
-            </div>
-        </div>
-    </footer>     
-</div>
+</div>  
+    <br />
+    <br />
+    <?php include 'footer.php'; ?>
+    
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="assets/js/classie.js"></script>
+    <script src="assets/js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="assets/js/jqBootstrapValidation.js"></script>
+    <script src="assets/js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="assets/js/freelancer.js"></script>
 </body>
 </html>
