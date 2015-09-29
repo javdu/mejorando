385352e4dc0aba4425653c666c050befbd8d15af
luis210783 <luis210783@gmail.com>
2015-09-29 19:21:34 -0300

    mensaje

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -2,10 +2,14 @@
 <header>
     <div class="container">
         <div class="row">
+            <img src="assets/img/fundacionPass_logoTipo.jpg">
             <div class="col-lg-12">  
                 <div class="intro-text">
-                    <div class="intro-lead-in">Bienvenidos a nuestra encuesta online!</div>
-                    <div class="intro-heading">MEJORANDO MENTES</div>
+                    
+                    <br />
+                    <br />
+                    <div class="intro-lead-in">Bienvenidos a</div>
+                    <div class="intro-heading">MENTES MEJORANDO ONLINE</div>
                     <br />
                     <a href="preguntas" class="btn btn-default btn-lg center-block" style="padding: 20px 40px; width: 200px; color: #0E6E8C;">EMPEZAR</a>
                 </div>
